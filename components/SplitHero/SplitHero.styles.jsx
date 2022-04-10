cd67396e508cf086845fc68e2e453d6e3d46a0e8
Lothar van Mourik <lothar@bravoure.nl>
2022-04-10 14:19:08 +0200

    feat(mobile hero): mobile hero

--- components/SplitHero/SplitHero.styles.jsx
+++ components/SplitHero/SplitHero.styles.jsx
@@ -1,7 +1,6 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.div`
-  height: 90vh;
   display: flex;
   flex-direction: column;
   background: var(--color-primary-300);
@@ -12,15 +11,26 @@ export const Wrapper = styled.div`
   }
 
   @media (${({ theme }) => theme.respondTo.desktop}) {
+    height: 90vh;
     flex-direction: row;
+  }
 
-    > *:first-child {
-      position: relative;
+  > *:first-child {
+    position: relative;
+    width: 100%;
+    height: 50vh;
+
+    @media (${({ theme }) => theme.respondTo.desktop}) {
+      height: auto;
       width: 40%;
     }
+  }
 
-    > *:nth-child(2) {
-      flex: 1;
+  > *:nth-child(2) {
+    flex: 1;
+    padding: 2.4rem 1.6rem;
+
+    @media (${({ theme }) => theme.respondTo.desktop}) {
       padding: 10rem 5vw;
     }
   }
