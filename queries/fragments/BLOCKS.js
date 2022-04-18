5a89e03fc60c29c8e89aed780b40dffd5b73960a
Lothar van Mourik <lothar@bravoure.nl>
2022-04-18 20:53:41 +0200

    feat(blocks): blocks as reusable function

--- queries/fragments/BLOCKS.js
+++ queries/fragments/BLOCKS.js
@@ -1,14 +1,63 @@
-import { gql } from '@apollo/client';
-import { IMAGES } from './IMAGES';
-
-export const BLOCKS = gql`
-  fragment blocks on MatrixBlockInterface {
-    id
-    typeHandle
-    ... on blocks_image_BlockType {
-      image {
-        ${IMAGES('hero')}
+export const BLOCKS = () =>
+  `
+    __typename
+    ... on AudienceBodyTitle__text___cta {
+      type
+      primary {
+        title_text_cta_title
+        title_text_cta_text
+        title_text_cta_text_columns
+        title_text_cta_cta_title
+        title_text_cta_cta_link {
+          _linkType
+          ... on _ExternalLink {
+            url
+            target
+          }
+          ... on _Document {
+            _meta {
+              uid
+              id
+            }
+          }
+        }
       }
     }
-  }
+    ... on AudienceBodyQuote {
+      type
+      primary {
+        text
+        author
+      }
+    }
+    ... on AudienceBodyVideo {
+      type
+      primary {
+        video_title
+        video_text
+        video_source
+        video_id
+      }
+    }
+    ... on AudienceBodyAccordion {
+      type
+      primary {
+        accordion_title
+        accordion_text
+      }
+      fields {
+        accordion_item_title
+        accordion_item_text
+      }
+    }
+    ... on AudienceBodyImages {
+      type
+      primary {
+        image_slider_sizing
+      }
+      fields {
+        image_slider_image
+        description
+      }
+    }
 `;
--- queries/singles/AUDIENCE_QUERY.js
+++ queries/singles/AUDIENCE_QUERY.js
@@ -1,4 +1,5 @@
 import { gql } from '@apollo/client';
+import { BLOCKS } from '../fragments/BLOCKS';
 
 export const AUDIENCE_QUERY = gql`
   query ($uid: String!, $lang: String! = "nl-nl") {
@@ -15,66 +16,7 @@ export const AUDIENCE_QUERY = gql`
             uid
           }
           body {
-            ... on AudienceBodyTitle__text___cta {
-              type
-              primary {
-                title_text_cta_title
-                title_text_cta_text
-                title_text_cta_text_columns
-                title_text_cta_cta_title
-                title_text_cta_cta_link {
-                  _linkType
-                  ... on _ExternalLink {
-                    url
-                    target
-                  }
-                  ... on _Document {
-                    _meta {
-                      uid
-                      id
-                    }
-                  }
-                }
-              }
-            }
-            ... on AudienceBodyQuote {
-              type
-              primary {
-                text
-                author
-              }
-            }
-            ... on AudienceBodyVideo {
-              type
-              primary {
-                video_title
-                video_text
-                video_source
-                video_id
-              }
-            }
-            ... on AudienceBodyAccordion {
-              type
-              primary {
-                accordion_title
-                accordion_text
-              }
-              fields {
-                accordion_item_title
-                accordion_item_text
-              }
-            }
-            ... on AudienceBodyImages {
-              type
-              primary {
-                image_slider_sizing
-              }
-              fields {
-                image_slider_image
-                description
-              }
-            }
-            __typename
+            ${BLOCKS()}
           }
         }
       }
