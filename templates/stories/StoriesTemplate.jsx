ba8f5bda30d8a18692acab9125f126bb8175a6cc
Rens Hiemstra <rens@studiodrieluik.nl>
2022-06-24 12:50:12 +0200

    Improve filter stories

--- templates/stories/StoriesTemplate.jsx
+++ templates/stories/StoriesTemplate.jsx
@@ -5,10 +5,15 @@ import { Container } from '../../components/Container/Container.styles';
 import { Hero } from '../../components/Hero/Hero';
 import Story from '../../components/Story/Story';
 import { Content, Filter, Wrapper } from './StoriesTemplate.styles';
-import { useRouter } from 'next/router';
+import { useRouter, query } from 'next/router';
+import { useEffect, useState } from 'react';
+import { motion } from 'framer-motion';
 
-export const StoriesTemplate = ({ stories, selectedCategory, setSelectedCategory }) => {
-  const router = useRouter()
+export const StoriesTemplate = ({ stories }) => {
+  const [selectedCategory, setSelectedCategory] = useState(null);
+  const [filteredStories, setFilteredStories] = useState(stories);
+  const [isLoading, setIsLoading] = useState(false);
+  const router = useRouter();
   const meta = {
     seoTitle: 'Verhalen',
   };
@@ -28,14 +33,36 @@ export const StoriesTemplate = ({ stories, selectedCategory, setSelectedCategory
     },
   ];
 
-  const handleClick = (category) => {
+  useEffect(() => {
+    if (!router.isReady) return;
+    if (router.asPath.includes('?type=')) {
+      const queryCapitalized = query.type.charAt(0).toUpperCase() + query.type.slice(1);
+      setSelectedCategory(queryCapitalized);
+    } else {
+      setSelectedCategory(null);
+    }
+  }, [router.isReady]);
+
+  useEffect(() => {
+    const newResults = stories.filter(story => {
+      if (selectedCategory !== null) {
+        return story.node.category === selectedCategory;
+      }
+      return stories;
+    });
+    setIsLoading(false);
+    setFilteredStories(newResults);
+  }, [selectedCategory]);
+
+  const handleClick = category => {
+    setIsLoading(true);
     setSelectedCategory(category);
     if (category !== null) {
       router.push(`/verhalen?type=${category.toLowerCase()}`);
     } else {
-      router.push('/verhalen')
+      router.push('/verhalen');
     }
-  }
+  };
 
   return (
     <Layout>
@@ -56,7 +83,7 @@ export const StoriesTemplate = ({ stories, selectedCategory, setSelectedCategory
                   Alles
                 </button>
               </li>
-              {categories.map(({category, title}, i) => (
+              {categories.map(({ category, title }, i) => (
                 <li key={i}>
                   <button
                     onClick={() => handleClick(category)}
@@ -65,16 +92,25 @@ export const StoriesTemplate = ({ stories, selectedCategory, setSelectedCategory
                   >
                     {title}
                   </button>
-                  {/* <Route path={`/verhalen?type=${category}`}>{title}</Route> */}
                 </li>
               ))}
             </ul>
           </Filter>
-          <Content>
-            {stories.map(({ node: group }) => (
-              <Story compact key={group._meta.uid} data={group} />
-            ))}
-          </Content>
+          {filteredStories && (
+            <Content>
+              {!isLoading &&
+                filteredStories.map(({ node: group }) => (
+                  <motion.div
+                    key={group._meta.uid}
+                    initial={{ y: 40, opacity: 0 }}
+                    animate={{ y: 0, opacity: 1 }}
+                    transition={{ ease: [0.16, 1, 0.3, 1], duration: 1, delay: 0.2 }}
+                  >
+                    <Story compact data={group} />
+                  </motion.div>
+                ))}
+            </Content>
+          )}
         </Wrapper>
       </Container>
     </Layout>
